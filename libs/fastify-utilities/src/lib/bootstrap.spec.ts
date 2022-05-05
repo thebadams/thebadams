a880d6ba73c9d2d2a8bdc9fc13aa74287b962a82
Brian Adams <bpeteradams@gmail.com>
2022-05-04 20:12:14 -0400

    Bootstrap(test): Wrote a spec file for bootstrap function and got it to register

---
+++ libs/fastify-utilities/src/lib/bootstrap.spec.ts
@@ -0,0 +1,8 @@
+import { bootStrap } from "./bootstrap";
+import { expect } from "chai";
+
+describe("Bootstrap function", function () {
+  it("Should run the test", function () {
+    expect(true).to.equal(true);
+  });
+});
