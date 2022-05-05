dc72830a57e1cb6293ebaa36a36494e5ae33a52f
Brian Adams <bpeteradams@gmail.com>
2022-05-04 21:26:31 -0400

    Testing(Bootstrap): Wrote a small testing suite for the bootstrap func

--- libs/fastify-utilities/src/lib/bootstrap.spec.ts
+++ libs/fastify-utilities/src/lib/bootstrap.spec.ts
@@ -1,8 +1,29 @@
-import { bootStrap } from "./bootstrap";
-import { expect } from "chai";
+import { bootStrap, RegisterFastifyServerFunction } from "./bootstrap.js";
+import { expect, assert } from "chai";
+import sinon from "sinon";
+import Sinon from "sinon";
+import { FastifyInstance } from "fastify";
+sinon.assert.expose(assert, { prefix: "" });
+const register = async () => {
+  return {
+    listen: function (port: string | number) {
+      return;
+    },
+  } as unknown as Partial<FastifyInstance>;
+};
 
+const callback = sinon.spy(register) as Sinon.SinonSpy;
+let callBackReturn: Sinon.SinonSpy;
+before(async function () {
+  const registerResult = await register();
+  callBackReturn = sinon.spy(registerResult.listen);
+  bootStrap(callback, 3000);
+});
 describe("Bootstrap function", function () {
-  it("Should run the test", function () {
-    expect(true).to.equal(true);
+  it("Should call a Passed Register Function", function () {
+    callback.calledOnce;
+  });
+  it("Should Call The listen function with 3000", function () {
+    callBackReturn.calledOnceWith(3000);
   });
 });
